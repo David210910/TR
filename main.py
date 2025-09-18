74043de725d5bc337a213fd6e8ec8da241fe5790
David210910 <k4letspley@gmail.com>
2025-09-18 17:28:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 
 
-bot = Bot(token=os.getenv('BOT_TOKEN', ''))
+bot = Bot(token=os.getenv('BOT_TOKEN', '8352932956:AAGM_8H6vnNGpd4oIBfjvFOvu-RpfP8TdYI'))
 dp = Dispatcher(storage=MemoryStorage())
 
 
@@ -75,4 +75,5 @@ async def main():
 
 
 if __name__ == '__main__':
+
     asyncio.run(main())
